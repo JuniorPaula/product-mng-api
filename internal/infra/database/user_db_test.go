d13325d0c47b2336aa4472ad0584f140ab0b7ad5
Junior Paula <luke.junnior@icloud.com>
2024-04-08 16:12:48 -0300

    test: ensure get by email succeeds

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -29,3 +29,24 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.NotNil(t, userFound.Password)
 }
+
+func TestGetByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.AutoMigrate(&entity.User{})
+
+	user, _ := entity.NewUser("Jhon Doe", "j@j.com", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.GetByEmail("j@j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
