ef4b2aeb7064835fa1714b34b2d3c998cff79281
Junior Paula <luke.junnior@icloud.com>
2024-04-12 08:02:24 -0300

    feat: create product db

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserInterface interface {
 	Create(user *entity.User) error
 	GetByEmail(email string) (*entity.User, error)
 }
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	GetAll(page, limit int, sort string) ([]entity.Product, error)
+	GetByID(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
