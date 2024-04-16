02e8dbb0a6fafe610bfcdf7234dfd6d1968ddcd5
Junior Paula <luke.junnior@icloud.com>
2024-04-16 08:54:51 -0300

    test: create tests for find all products with pagination

---
+++ internal/infra/database/product_db_test.go
@@ -0,0 +1,65 @@
+package database
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"web_server/internal/entity"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateNewProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10)
+	assert.Nil(t, err)
+
+	productDB := NewProduct(db)
+	err = productDB.Create(product)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, product.ID)
+}
+
+func TestFindAllProducts(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+
+	for i := 1; i < 24; i++ {
+		product, err := entity.NewProduct(fmt.Sprintf("Product %d", i), rand.Float64()*100)
+		assert.Nil(t, err)
+		db.Create(product)
+		assert.NoError(t, err)
+	}
+
+	productDB := NewProduct(db)
+	products, err := productDB.GetAll(1, 10, "asc")
+	assert.NoError(t, err)
+
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 1", products[0].Name)
+	assert.Equal(t, "Product 10", products[9].Name)
+
+	products, err = productDB.GetAll(2, 10, "asc")
+	assert.NoError(t, err)
+
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 11", products[0].Name)
+	assert.Equal(t, "Product 20", products[9].Name)
+
+	products, err = productDB.GetAll(3, 10, "asc")
+	assert.NoError(t, err)
+
+	assert.Len(t, products, 3)
+	assert.Equal(t, "Product 21", products[0].Name)
+	assert.Equal(t, "Product 23", products[2].Name)
+}
