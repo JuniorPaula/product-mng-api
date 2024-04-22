dcf3e9d893d60943920b146a1f00731876ce4d81
Junior Paula <luke.junnior@icloud.com>
2024-04-22 18:18:19 -0300

    feat: implement handler to create a product

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,68 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"web_server/configs"
+	"web_server/internal/dto"
+	"web_server/internal/entity"
+	"web_server/internal/infra/database"
+
+	"github.com/goccy/go-json"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	cfg, _ := configs.LoadConfig(".")
-	fmt.Println(cfg.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("teste.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productHandler := NewProductHandler(db)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	fmt.Print("Server running on port 8000\n")
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db *gorm.DB) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: database.NewProduct(db),
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(p)
 }
